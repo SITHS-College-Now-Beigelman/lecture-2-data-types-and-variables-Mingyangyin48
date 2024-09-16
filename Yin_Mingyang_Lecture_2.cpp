3bc8e030aa6751f9f20ce7f6ee7370a914551d40
Mingyangyin48 <mingyangy@nycstudents.net>
2024-09-16 16:27:58 -0400

    lecture 2

---
+++ Yin_Mingyang_Lecture_2.cpp
@@ -0,0 +1,32 @@
+// Mingyang Michelle Yin
+// Lecture 2
+// Sept 16, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+    int newNum = 10;
+    cout << "int: " << newNum << endl;
+
+    float newFlo = 20.11;
+    cout << "float: " << newFlo << endl;
+
+    double douNum  = 120.11;
+    cout << "double: " << douNum << endl;
+
+    char newCha = '!';
+    cout << "char: " << newCha << endl;
+
+    bool newBo = true;
+    cout << "bool: " << newBo << endl;
+
+    string newStr = "yello";
+    cout << "string: " << newStr << endl;
+
+
+    int inches;
+
+    return 0;
+}
