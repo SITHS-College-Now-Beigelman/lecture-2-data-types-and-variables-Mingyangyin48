58a95813012789b69b1741020309eb2dd3805c7b
Mingyangyin48 <mingyangy@nycstudents.net>
2024-09-17 16:02:47 -0400

    Lecture 2

--- Yin_Mingyang_Lecture_2.cpp
+++ Yin_Mingyang_Lecture_2.cpp
@@ -7,26 +7,63 @@
 using namespace std;
 
 int main() { 
-    int newNum = 10;
-    cout << "int: " << newNum << endl;
+    // setting values 
 
-    float newFlo = 20.11;
-    cout << "float: " << newFlo << endl;
+    int newNum = 10; // setting a value for a whole number
+    float newFlo = 20.11; // setting a value for a floating point number 
+    double douNum  = 120.11; // setting a value for a larger floating point number 
+    char newCha = '!'; // setting a variable for a charater
+    bool newBo = true; // setting a true or false for boolean
+    string newStr = "yello"; // setting a word for the string
 
-    double douNum  = 120.11;
-    cout << "double: " << douNum << endl;
+    // print values 
+    
+    cout << "int: " << newNum << endl; // printing value for a whole number 
+    cout << "float: " << newFlo << endl; // printing value for a floating point number
+    cout << "double: " << douNum << endl; // printing value for a larger floating point number 
+    cout << "char: " << newCha << endl; // printing value for charater
+    cout << "bool: " << newBo << endl; // printing value for boolean
+    cout << "string: " << newStr << endl; // printing value for string
 
-    char newCha = '!';
-    cout << "char: " << newCha << endl;
+    // slide 37 
 
-    bool newBo = true;
-    cout << "bool: " << newBo << endl;
+    int inches = 100; // setting a value for inches
+    
+    cout << inches << " inch(es) = "; // printing the words inch(es) =
+    cout << inches / 12 << " feet (foot) and "; // finding the amount of feets
+    cout << inches % 12 << " inch(es) " << endl; // finding the amount of the remainder of inches
 
-    string newStr = "yello";
-    cout << "string: " << newStr << endl;
+    // Compare 2 charaters 
+
+    char chaA = '*'; // first charater 
+    char chaB = '@'; // second charater
+    
+    cout << "Is * < @ = " << (chaA<chaB) << endl; // printing if * is less than @; and seeing if it was true or false
+
+    // equation 
+
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl; // solving 2+3.5
+    cout << "6 / 4 + 3.9 = " << 6 / 4 + 3.9 << endl; // solving 6 / 4 + 3.9
+    cout << "5.4 * 2 - (13.6 + 18 / 2) = " << 5.4 * 2 - (13.6 + 18 / 2) << endl; // 5.4 * 2 - (13.6 + 18 / 2)
 
 
-    int inches;
 
     return 0;
 }
+
+    /* 
+    OUTPUT  
+
+    int: 10
+    float: 20.11
+    double: 120.11
+    char: !
+    bool: 1
+    string: yello
+    100 inch(es) = 8 feet (foot) and 4 inch(es)
+    Is * < @ = 1
+    2 + 3.5 = 5.5
+    6 / 4 + 3.9 = 4.9
+    5.4 * 2 - (13.6 + 18 / 2) = -11.8
+
+     */
